0708a9a3d8967f7f91aab27fea9caecac4b5586a
Pranshu Nayak <pranshunayak566@gmail.com>
2023-12-30 22:57:17 +0530

    modified whatsapp uri

--- src/components/Contact/Whatsapp.jsx
+++ src/components/Contact/Whatsapp.jsx
@@ -9,7 +9,7 @@ function Whatsapp() {
       class="fixed z-90 bottom-10 right-8 bg-blue-600 w-20 h-20 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-blue-700 hover:drop-shadow-2xl hover:animate-bounce duration-300"
     >
       <a
-        href="https:/wa.me/916352932225"
+        href="https://wa.me/916352932225"
         target="_blank"
         class="whatsapp_float"
       >
